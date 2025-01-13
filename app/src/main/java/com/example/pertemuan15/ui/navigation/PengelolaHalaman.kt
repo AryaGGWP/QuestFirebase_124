b78d3e6e96488d86e13abf064f7bac2fe4eda8c7
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-13 20:19:09 +0700

    Add PengelolaHalaman composable dengan navigation menggunakan NavHost serta import dependencies yang diperlukan

--- app/src/main/java/com/example/pertemuan15/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan15/ui/navigation/PengelolaHalaman.kt
@@ -2,13 +2,36 @@ package com.example.pertemuan15.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan15.ui.home.pages.HomeScreen
+import com.example.pertemuan15.ui.insert.pages.InsertMhsView
 
 @Composable
-fun PengelolaHalamann(
+fun PengelolaHalaman(
     modifier: Modifier,
     navController: NavHostController = rememberNavController()
 ){
-
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
 }
+
