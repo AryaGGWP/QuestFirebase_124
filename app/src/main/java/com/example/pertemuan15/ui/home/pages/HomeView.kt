f39fc8a79e3da938931c97bc2a49d48d3347bddd
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-13 20:23:43 +0700

    Add MahasiswaApplication

--- app/src/main/java/com/example/pertemuan15/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/pertemuan15/ui/home/pages/HomeView.kt
@@ -36,7 +36,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan15.model.Mahasiswa
