5b23b862d424657c2a82dee22eb34acc93629a59
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-13 20:06:52 +0700

    Add function PengelolaHalaman sebagai navigator

---
+++ app/src/main/java/com/example/pertemuan15/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan15.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalamann(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+
+}
