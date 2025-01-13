c0b9310b47ba5c9ac20ea6905a4860c2870db158
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-13 13:56:04 +0700

    ADD - function insert mahasiswa

--- app/src/main/java/com/example/pertemuan15/repo/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan15/repo/NetworkRepositoryMhs.kt
@@ -3,24 +3,30 @@ package com.example.pertemuan15.repo
 import com.example.pertemuan15.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs (
     private val firestore: FirebaseFirestore
-):RepositoryMhs{
-    override suspend fun  insertMhs(mahasiswa: Mahasiswa){
-        firestore.collection("Mahasiswa").add(mahasiswa)
+): RepositoryMhs {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
         val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("", Query.Direction.ASCENDING)
+            .orderBy("nim", Query.Direction.ASCENDING)
             .addSnapshotListener{value, error ->
                 if (value != null){
                     val mhslist = value.documents.mapNotNull {
-                        it.toObject(Mahasiswa::class.java)
+                        it.toObject(Mahasiswa::class.java)!!
                     }
                     trySend(mhslist)
                 }
@@ -30,15 +36,14 @@ class NetworkRepositoryMhs (
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow {
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
         TODO()
     }
 
-    override suspend fun deleteMhs(mahasiswa: Mahasiswa) { //menghapus data mahasiswa
-
-    }
-
-    override suspend fun updateMhs(mahasiswa: Mahasiswa) { //memperbarui data mahasiswa dalam database
-
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        TODO()
     }
 }
